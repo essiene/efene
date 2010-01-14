0ef511f190bafa8175e10eb90a2537a22b57d998
Essien Ita Essien <essiene@gmail.com>
2010-01-14 18:34:06 +0100

    First pass at implementing getopt option parsing
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/tools/fnc.c
+++ src/tools/fnc.c
@@ -1,3 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+
+#define OPTSTRING "+ho:t:v"
+#define MAX_STRLEN 255
+
+int main(int argc, char** argv)
+{
+    int flag_help= 0;
+    int flag_verbose = 0;
+    char* output_dir = NULL;
+    char* output_type = NULL;
+
+    int ret_status = 0;
+
+    while(1) {
+        int c = getopt(argc, argv, OPTSTRING);
+
+        switch(c) {
+            case 'h': flag_help = 1; break;
+            case 'v': flag_verbose = 1; break;
+            case 'o': output_dir = strndup(optarg, MAX_STRLEN); break;
+            case 't': output_type = strndup(optarg, MAX_STRLEN); break;
+        }
+
+        if(c == -1) {
+            break;
+        }
+
+    }
+
+        if(flag_help) {
+            printf("help is on\n");
+        }
+
+        if(flag_verbose) {
+            printf("verbose is on\n");
+        }
+
+        if(output_dir) {
+            printf("output_dir = %s\n", output_dir);
+        }
+
+        if(output_type) {
+            printf("output_type = %s\n", output_type);
+        }
+
+
+
+exit:
+    free(output_dir);
+    free(output_type);
+    return ret_status;
+}
